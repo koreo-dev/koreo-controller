90da8e23913dfba5faa935e9e3821b18039de7fd
Eric Larssen <elarssen91@gmail.com>
2025-03-20 14:12:59 -0500

    Fix order of imports

--- src/controller/custom_workflow.py
+++ src/controller/custom_workflow.py
@@ -9,8 +9,8 @@ import kr8s
 from koreo.result import Retry, is_error, is_unwrapped_ok, raise_for_error
 
 from koreo.cache import get_resource_from_cache
-from koreo.conditions import Condition, update_condition
 from koreo.cel.encoder import convert_bools
+from koreo.conditions import Condition, update_condition
 from koreo.workflow.reconcile import reconcile_workflow
 from koreo.workflow.registry import get_custom_crd_workflows
 from koreo.workflow.structure import Workflow
